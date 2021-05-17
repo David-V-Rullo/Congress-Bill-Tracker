9aba876a31f892856c7b7e95f7c360672897428c
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-17 16:53:28 -0400

    Fiddling with data. Fetch seems to be functioning.

--- assets/js/bill-status.js
+++ assets/js/bill-status.js
@@ -1,33 +1,26 @@
-function renderBillStatus () {
-    $(".status-button").each(function (){
-        console.log($(this).attr("id"))
+function renderBillStatus() {
+  $(".status-button").each(function () {
+    console.log($(this).attr("id"));
     //  Looks at data and gets back status code
-    //  If the status code matches the button ID, light that button. //   All buttons should have default of grey. 
-    })
-   
+    //  If the status code matches the button ID, light that button. //   All buttons should have default of grey.
+  });
 }
-
-renderBillStatus()
+function renderBillEl(data) {
+  renderBillStatus();
+}
 
 function dataSet() {
-    var url = "https://api.propublica.org/congress/v1/bills/search.json";
-    fetch(url, {
-        headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
+  var url = "https://api.propublica.org/congress/v1/bills/search.json";
+  fetch(url, {
+    headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" },
+  })
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
     })
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-            return response.json();
-        })
-        .then(function (response) {
-           return response
-        })
-        
+    .then(function (response) {
+      renderBillEl(response);
+    });
 }
-var data = dataSet()
-console.log(data)
-
-
-
-
