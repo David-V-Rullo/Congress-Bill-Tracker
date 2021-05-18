27c84e8fdd8ef2867b27abe3ce0e0665d2736d86
Patrick Braun <prbraun@protonmail.com>
2021-05-18 09:07:17 -0500

    Print some data from propublica api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
-//TEST
-console.log("test")
-console.log("test2")
+console.log("Entering script.js");
+var billTextEl = $('#bill-text');
+var sponsorEl = $('#sponsor').children();
+
+function printResult(result)
+{
+    console.log(result.sponsor_name);
+}
+
+function fetchPropublicaApi() {
+    var url = "https://api.propublica.org/congress/v1/bills/search.json";
+
+    fetch(url, {
+        headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (locRes) {
+            for(var i = 0; i < locRes.results[0].bills.length; i++){
+                printResult(locRes.results[0].bills[i]);
+            }
+        })
+}
+
+fetchPropublicaApi();
