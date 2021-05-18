134013a41088f77445016742eed4d1b6056d41b2
Patrick Braun <prbraun@protonmail.com>
2021-05-18 13:26:54 -0500

    Trying to paginate api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,69 +2,13 @@ console.log("Entering script.js");
 var billTextEl = $('#bill-text');
 var sponsorEl = $('#sponsor').children();
 
-// function printResult(result)
-// {
-//     console.log(result.sponsor_name);
-// }
-
-// function fetchPropublicaApi() {
-//     var url = "https://api.propublica.org/congress/v1/bills/search.json";
-
-//     fetch(url, {
-//         headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
-//     })
-//         .then(function (response) {
-//             if (!response.ok) {
-//                 throw response.json();
-//             }
-//             return response.json();
-//         })
-//         .then(function (locRes) {
-//             for(var i = 0; i < locRes.results[0].bills.length; i++){
-//                 printResult(locRes.results[0].bills[i]);
-//             }
-//         })
-// }
-
-// fetchPropublicaApi();
-
-//var billTextEl= document.getElementById("bill-text")
-
-
-
-var billOutput = {};
-var billData;
-
-//https://api.propublica.org/congress/v1/{chamber}/votes/{start-date}/{end-date}.json
-//https://api.propublica.org/congress/v1/{congress}/{chamber}/bills/{type}.json
-
-function test() {
-    var url = "https://api.propublica.org/congress/v1/117/both/bills/introduced.json?offset=0";
-    fetch(url, {
-        headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
-    })
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-            return response.json();
-        })
-        .then(function (locRes) {
-            billOutput = (JSON.stringify(locRes))
-            billData = (JSON.parse(billOutput))
-            var billNewest = billData.results[0].bills[0].bill_id + " " + billData.results[0].bills[0].sponsor_name + " " + billData.results[0].bills[0].short_title;
-            billTextEl.innerText = billNewest
-            console.log(billData.results[0]);
-        })
-}
-
 var current = moment().format("YYYY-MM-DD");
 var oneWeek = moment().subtract(2, 'days').format('YYYY-MM-DD');
 var oneMonth = moment().subtract(1, 'months').format('YYYY-MM-DD');
 
 function billsByDate(date = oneWeek) {
     var offset = 0;
-    var good = 5;
+    var good = 10;
     while (good > 0) {
         console.log(good);
         var url = "https://api.propublica.org/congress/v1/117/both/bills/introduced.json?offset=".concat(offset);
@@ -87,8 +31,6 @@ function billsByDate(date = oneWeek) {
                     }
                     else{
                         console.log("TOO EARLY");
-                        good = 0; 
-                        break;
                     }
                 }
             })
