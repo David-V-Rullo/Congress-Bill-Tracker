a71cbf49a59d1f3fe40ec1a45020a1aaf309d6e3
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-17 21:26:08 -0400

    JSON query on propublica

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
-//TEST
-console.log("test")
-console.log("test2")
+
+
+
+
+var billOutput={};
+var billData;
+
+
+function test() {
+    var url = "https://api.propublica.org/congress/v1/bills/search.json";
+    fetch(url, {
+        headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (locRes) {
+            console.log(JSON.stringify(locRes))
+            
+            billOutput=(JSON.stringify(locRes))
+            console.log(Object.keys(locRes))
+            console.log(JSON.parse(billOutput))
+            billData=(JSON.parse(billOutput))
+            
+        })
+}
+test();
