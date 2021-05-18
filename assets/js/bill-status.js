0d07dfc960c60582896264fd0abe4f776ce8c004
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-18 11:30:29 -0400

    Working on getting search buttons working

--- assets/js/bill-status.js
+++ assets/js/bill-status.js
@@ -1,3 +1,6 @@
+var topic = $("#topic option:selected").index()
+
+console.log(topic)
 function renderBillStatus() {
   $(".status-button").each(function () {
     console.log($(this).attr("id"));
@@ -9,18 +12,48 @@ function renderBillEl(data) {
   renderBillStatus();
 }
 
-function dataSet() {
-  var url = "https://api.propublica.org/congress/v1/bills/search.json";
-  fetch(url, {
-    headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" },
-  })
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
-      return response.json();
-    })
-    .then(function (response) {
-      renderBillEl(response);
-    });
-}
+// function dataSet() {
+//   var url = "https://api.propublica.org/congress/v1/bills/search.json";
+//   fetch(url, {
+//     headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" },
+//   })
+//     .then(function (response) {
+//       if (!response.ok) {
+//         throw response.json();
+//       }
+//       return response.json();
+//     })
+//     .then(function (response) {
+//       renderBillEl(response);
+//     });
+// }
+
+var billTextEl= document.getElementById("bill-text")
+
+
+var billOutput={};
+var billData;
+
+// function test() {
+//     var url = "https://api.propublica.org/congress/v1/bills/search.json";
+//     fetch(url, {
+//         headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
+//     })
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 throw response.json();
+//             }
+//             return response.json();
+//         })
+//         .then(function (locRes) {
+//             console.log(JSON.stringify(locRes))
+            
+//             billOutput=(JSON.stringify(locRes))
+//             console.log(Object.keys(locRes))
+//             console.log(JSON.parse(billOutput))
+//             billData=(JSON.parse(billOutput))
+//             billNewest=billData.results[0].bills[0].bill_id + " " + billData.results[0].bills[0].sponsor_name + " " + billData.results[0].bills[0].short_title;
+//             billTextEl.innerText=billNewest
+//         })
+// }
+// test();
