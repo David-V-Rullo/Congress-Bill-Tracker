de8ef9d6bd38e69266c1f9f43fba36627b217ca1
Patrick Braun <prbraun@protonmail.com>
2021-05-18 19:30:52 -0500

    Got new HTML from drullo branch. Created a display area for datesearch results(supposed to start hidden)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,57 @@
 console.log("Entering script.js");
-var billTextEl = $('#bill-text');
-var sponsorEl = $('#sponsor').children();
+var dateSearchResultsEl = $("#date-search-results");
+var searchBtn = $('#search-submit');
+var previousBtn = $('#previous');
+var nextBtn = $('#next');
 
 var current = moment().format("YYYY-MM-DD");
-var oneWeek = moment().subtract(2, 'days').format('YYYY-MM-DD');
+var oneWeek = moment().subtract(7, 'days').format('YYYY-MM-DD');
 var oneMonth = moment().subtract(1, 'months').format('YYYY-MM-DD');
+var offset = 0;
 
 function billsByDate(date = oneWeek) {
-    var offset = 0;
-    var good = 10;
-    while (good > 0) {
-        console.log(good);
-        var url = "https://api.propublica.org/congress/v1/117/both/bills/introduced.json?offset=".concat(offset);
-        console.log(url);
-        fetch(url, {
-            headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
+    dateSearchEl.show();
+
+    var url = "https://api.propublica.org/congress/v1/bills/search.json?sort=date&dir=desc&offset=".concat(offset);
+    console.log("Offset: " + offset);
+    fetch(url, {
+        headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
         })
-            .then(function (response) {
-                if (!response.ok) {
-                    throw response.json();
-                }
-                return response.json();
-            })
-            .then(function (response) {
-                console.log(response.results[0].bills);
-                for(i = 0; i < response.results[0].bills.length; i++){
-                    console.log(date);
-                    if(moment(response.results[0].bills[i].introduced_date).isAfter(date)){
-                        console.log("Bill id: " + response.results[0].bills[i].bill_id + " Date: " + response.results[0].bills[i].introduced_date + " Is after: " + date);
-                    }
-                    else{
-                        console.log("TOO EARLY");
-                    }
-                }
-            })
-        offset = offset + 20;
-        good--;
-    }
+        .then(function (response) {
+            console.log(response.results[0].bills);
+            for (i = 0; i < response.results[0].bills.length; i++) {
+                console.log(response.results[0].bills[i].latest_major_action_date);
+                // if(moment(response.results[0].bills[i].introduced_date).isAfter(date)){
+                //     console.log("Bill id: " + response.results[0].bills[i].bill_id + " Date: " + response.results[0].bills[i].introduced_date + " Is after: " + date);
+                // }
+                // else{
+                //     console.log("TOO EARLY");
+                // }
+            }
+        })
+    offset += 20;
 }
 
-billsByDate();
+dateSearchResultsEl.hide();
+searchBtn.on('click', function(){
+    offset = 0;
+    billsByDate();
+});
+
+nextBtn.on('click', function(){
+    offset += 20;
+    billsByDate();
+});
+
+previousBtn.on('click', function(){
+    if(offset >= 20){
+        offset -= 20;
+        billsByDate();
+    }
+});
