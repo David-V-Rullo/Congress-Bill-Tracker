15810e9d8a74fb1de921ec7c80da4bd5e85efba5
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-19 10:02:28 -0400

    work work on HTML and CSS. Getting ready for js integration

--- assets/js/bill-status.js
+++ assets/js/bill-status.js
@@ -46,52 +46,10 @@ function renderBillStatus() {
     //  If the status code matches the button ID, light that button. //   All buttons should have default of grey.
   });
 }
-function renderBillEl(data) {
-  renderBillStatus();
-}
-
-// function dataSet() {
-//   var url = "https://api.propublica.org/congress/v1/bills/search.json";
-//   fetch(url, {
-//     headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" },
-//   })
-//     .then(function (response) {
-//       if (!response.ok) {
-//         throw response.json();
-//       }
-//       return response.json();
-//     })
-//     .then(function (response) {
-//       renderBillEl(response);
-//     });
-// }
-
-var billTextEl= document.getElementById("bill-text")
 
 
-var billOutput={};
-var billData;
 
-// function test() {
-//     var url = "https://api.propublica.org/congress/v1/bills/search.json";
-//     fetch(url, {
-//         headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
-//     })
-//         .then(function (response) {
-//             if (!response.ok) {
-//                 throw response.json();
-//             }
-//             return response.json();
-//         })
-//         .then(function (locRes) {
-//             console.log(JSON.stringify(locRes))
-            
-//             billOutput=(JSON.stringify(locRes))
-//             console.log(Object.keys(locRes))
-//             console.log(JSON.parse(billOutput))
-//             billData=(JSON.parse(billOutput))
-//             billNewest=billData.results[0].bills[0].bill_id + " " + billData.results[0].bills[0].sponsor_name + " " + billData.results[0].bills[0].short_title;
-//             billTextEl.innerText=billNewest
-//         })
-// }
-// test();
+// var billTextEl= document.getElementById("bill-text")
+
+
+
