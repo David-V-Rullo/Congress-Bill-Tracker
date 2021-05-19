f47749f4125ca6f626f95994c20dd2b5683b1c0b
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-19 13:15:18 -0400

    wikipedia query done

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -15,33 +15,42 @@ var govInfoData;
 
 
 
-function getGovInfo() {
-    var url = "https://api.propublica.org/congress/v1/bills/search.json";
-    fetch(url, {
-        headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
-    })
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-            return response.json();
-        })
-        .then(function (govInfoLocRes) {
-            console.log(JSON.stringify(govInfoLocRes))
+// function getGovInfo() {
+//     var url = "https://en.wikipedia.org/w/api.php?action=parse&page=Pet_door&prop=text&formatversion=json&origin=*";
+//     fetch(url)
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 throw response.json();
+//             }
+//             return response.json();
+//         })
+//         .then(function (govInfoLocRes) {
+//             console.log(JSON.stringify(govInfoLocRes))
             
-            govInfoOutput=(JSON.stringify(govInfoLocRes))
-            console.log(Object.keys(govInfoLocRes))
-            console.log(JSON.parse(govInfoOutput))
-            govInfoData=(JSON.parse(govInfoOutput))
-            //query the results to pull what we want
-            govInfoSelected=govInfoData
-            //CHANGE FROM BILLTEXTEL
-            billTextEl.innerText=billInfo
-        })
-}
-getGovInfo();
+//             govInfoOutput=(JSON.stringify(govInfoLocRes))
+//             console.log(Object.keys(govInfoLocRes))
+//             console.log(JSON.parse(govInfoOutput))
+//             govInfoData=(JSON.parse(govInfoOutput))
+//             //query the results to pull what we want
+//             govInfoSelected=govInfoData
+//             //CHANGE FROM BILLTEXTEL
+//             billTextEl.innerText=billInfo
+//         })
+// }
+// getGovInfo();
 
 
+var wikiEndpoint = "https://en.wikipedia.org/w/api.php";
+var params = "action=parse&page=Chuck_Schumer&prop=text&formatversion=2&format=json";
+
+/**
+ * Send the request to get the images
+ */
+fetch(wikiEndpoint + "?" + params + "&origin=*")
+    .then(function(response){return response.json();})
+    .then(function(response) {
+         console.log(JSON.stringify(response))
+          });
 
 
 
