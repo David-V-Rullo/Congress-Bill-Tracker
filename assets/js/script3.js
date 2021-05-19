ef822ba4dc6e9a9f60a76b6c6b9f28ca81ca0d97
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-18 21:48:32 -0400

    search by topic functioning

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -11,20 +11,16 @@ var topics=document.getElementById("topic")
 //Get search results
 topics.addEventListener("change", select)
 function select(event) {
-
-    console.log(event)
+getKeyBill(event.target.value)
+    console.log(event.target.value)
 }
-
-
-//var userSearch = $("#topic:selected").attr("id")
-
 
 
 
 //Default get latest Bill
 
-function getKeyBill() {
-    var url = "https://api.propublica.org/congress/v1/bills/search.json?query=education";
+function getKeyBill(topic) {
+    var url = "https://api.propublica.org/congress/v1/bills/search.json?query=" + topic;
     fetch(url, {
         headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
     })
@@ -45,30 +41,3 @@ function getKeyBill() {
             billKeyTextEl.innerText=billKey
         })
 }
-getKeyBill();
-
-
-// function getKeyBill() {
-//     var url = "https://api.propublica.org/congress/v1/bills/search.json?query={query}";
-//     fetch(url, {
-//         headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" }
-//     })
-//         .then(function (response) {
-//             if (!response.ok) {
-//                 throw response.json();
-//             }
-//             return response.json();
-//         })
-//         .then(function (keyLocRes) {
-//             console.log(JSON.stringify(keyLocRes))
-            
-//             billKeyOutput=(JSON.stringify(keyLocRes))
-//             console.log(Object.keys(keyLocRes))
-//             console.log(JSON.parse(billKeyOutput))
-//             billKeyData=(JSON.parse(billKeyOutput))
-//             billKey=billKeyData.results[0].bills[0].bill_id + " " + billKeyData.results[0].bills[0].sponsor_name + " " + billKeyData.results[0].bills[0].short_title;
-//             billKeyTextEl.innerText=billKey
-//         })
-// }
-// getKeyBill();
-
